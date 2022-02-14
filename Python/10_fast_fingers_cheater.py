53df8ae2bbad12f94895019b614a9ec50beaadf3
AvivYunker <avivyunker@gmail.com>
2022-02-14 18:41:26 +0200

    create 10 fast-fingers cheater program in Python

---
+++ Python/10_fast_fingers_cheater.py
@@ -0,0 +1,26 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.alert import Alert
+browser = webdriver.Chrome()
+
+browser.get('https://10fastfingers.com')
+time.sleep(3)
+allow_all_cookies_button = browser.find_element_by_id('CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll')
+allow_all_cookies_button.click()
+
+start_typing_test_button = browser.find_element_by_id('typing-test')
+start_typing_test_button.click()
+time.sleep(2)
+long_list = browser.find_element_by_id('row1')
+all_words = list(long_list.find_elements_by_tag_name('span'))
+
+input_word_field = browser.find_element_by_id('inputfield')
+
+for cnt in range(0, len(all_words), 1):
+    word = all_words[cnt].text
+    input_word_field.send_keys(word + ' ')
+input_word_field.clear()
+
+#print("all_words is: " + str(all_words))
+time.sleep(20)
+browser.quit()
